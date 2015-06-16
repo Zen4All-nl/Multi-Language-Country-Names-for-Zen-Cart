bf592ea7860199fbce247570d4e8c11969c19f31
Erik <info@zen4all.nl>
2015-06-16 08:32:28 +0200

    Download id updated

--- 3_install/YOUR_ADMIN/includes/init_includes/init_multi_language_country_names.php
+++ 3_install/YOUR_ADMIN/includes/init_includes/init_multi_language_country_names.php
@@ -13,7 +13,7 @@ if (!defined('IS_ADMIN_FLAG')) {
 $module_constant = 'MULTI_LANGUAGE_COUNTRY_NAMES_VERSION'; // This should be a UNIQUE name followed by _VERSION for convention
 $module_installer_directory = DIR_FS_ADMIN . 'includes/installers/multi_language_country_names'; // This is the directory your installer is in, usually this is lower case
 $module_name = "Multi Language Country Names"; // This should be a plain English or Other in a user friendly way
-$zencart_com_plugin_id = 0; // from zencart.com plugins - Leave Zero not to check
+$zencart_com_plugin_id = 2006; // from zencart.com plugins - Leave Zero not to check
 //Just change the stuff above... Nothing down here should need to change
 
 
